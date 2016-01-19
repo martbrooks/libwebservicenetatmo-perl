057bdf573aed8a05c892daac8761c8d9b20fe0b3
Martin A. Brooks <martin@hinterlands.org>
2016-01-19 20:19:01 +0000

    token refresh logic. tidy.

--- lib/WebService/Netatmo/Common.pm
+++ lib/WebService/Netatmo/Common.pm
@@ -4,7 +4,6 @@ use v5.10;
 
 use Data::Dumper;
 use DateTime::Format::DateParse;
-use Exporter qw(import);
 use JSON::XS;
 use LWP::UserAgent;
 use YAML::XS qw(LoadFile DumpFile);
@@ -24,6 +23,7 @@ sub _get_or_refresh_token {
     my $self       = shift;
     my $now        = DateTime->now();
     my $tokenstore = $self->tokenstore;
+    my $haschanged = 0;
     my $store;
 
     if ( -e $tokenstore ) {
@@ -32,10 +32,23 @@ sub _get_or_refresh_token {
         my $lastupdate = DateTime::Format::DateParse->parse_datetime( $store->{token_last_updated} );
         my $diffsecs   = $now->epoch() - $lastupdate->epoch();
         _debug( $self, "Token is $diffsecs seconds old." );
+
+        # if ( $diffsecs > $store->{expires_in} ) {
+        if ( $diffsecs > 60 ) {
+            _debug( $self, 'Refreshing tokens.' );
+            $self->refresh_token( $store->{refresh_token} );
+            my $newstore = $self->__refresh_token();
+            $store->{access_token}       = $newstore->{access_token};
+            $store->{refresh_token}      = $newstore->{refresh_token};
+            $store->{token_last_updated} = $now->datetime();
+            $haschanged                  = 1;
+        }
+
     } else {
         _debug( $self, "Fetching new tokens." );
-        $store = __get_token($self);
+        $store                       = $self->__get_token();
         $store->{token_last_updated} = $now->datetime();
+        $haschanged                  = 1;
     }
 
     $self->access_token( $store->{access_token} );
@@ -43,7 +56,9 @@ sub _get_or_refresh_token {
     $self->token_expires( $store->{expires_in} );
     $self->token_last_updated( $store->{token_last_updated} );
 
-    DumpFile( "$tokenstore", $store );
+    if ( $haschanged == 1 ) {
+        DumpFile( "$tokenstore", $store );
+    }
 }
 
 sub __get_token {
@@ -69,6 +84,27 @@ sub __get_token {
     return $content;
 }
 
+sub __refresh_token {
+    my $self = shift;
+    my $ua   = LWP::UserAgent->new;
+    my $r    = $ua->post(
+        'https://api.netatmo.net/oauth2/token',
+        [
+            grant_type    => 'refresh_token',
+            client_id     => $self->client_id,
+            client_secret => $self->client_secret,
+            refresh_token => $self->refresh_token,
+        ]
+    );
+
+    unless ( $r->is_success ) {
+        die $r->status_line;
+    }
+
+    my $content = decode_json( $r->decoded_content );
+    return $content;
+}
+
 sub _debug {
     my ( $self, $message ) = @_;
     if ( $self->debug ) {
@@ -76,4 +112,5 @@ sub _debug {
         say "[$now] $message";
     }
 }
+
 1;
--- netatmo.pl
+++ netatmo.pl
@@ -12,23 +12,22 @@ use YAML::XS qw(LoadFile);
 use lib dirname( abs_path $0) . '/lib';
 use WebService::Netatmo::WeatherStation;
 
-
 my $yaml = LoadFile("settings.yaml");
 
 my $client_id     = $yaml->{client_id};
 my $client_secret = $yaml->{client_secret};
 my $username      = $yaml->{username};
 my $password      = $yaml->{password};
-my $tokenstore     = './tokenstore.yaml';
-my $debug = 1;
+my $tokenstore    = './tokenstore.yaml';
+my $debug         = 1;
 
-my $netatmo=WebService::Netatmo::WeatherStation->new(
-        client_id     => $client_id,
-        client_secret => $client_secret,
-        username      => $username,
-        password      => $password,
-        tokenstore    => $tokenstore,
-        debug         => $debug,
+my $netatmo = WebService::Netatmo::WeatherStation->new(
+    client_id     => $client_id,
+    client_secret => $client_secret,
+    username      => $username,
+    password      => $password,
+    tokenstore    => $tokenstore,
+    debug         => $debug,
 );
 
 print Dumper $netatmo;
