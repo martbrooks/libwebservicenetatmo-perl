58474aafc2ed271ec2f417b026a33dbb32056737
Martin A. Brooks <martin@hinterlands.org>
2016-01-19 20:32:47 +0000

    strip out libs.

--- lib/WebService/Netatmo/WeatherStation.pm
+++ lib/WebService/Netatmo/WeatherStation.pm
@@ -1,14 +1,7 @@
 package WebService::Netatmo::WeatherStation;
+
 use Moo;
-
-use Cwd qw(abs_path);
 use Data::Dumper;
-use DateTime::Format::DateParse;
-use File::Basename qw(dirname);
-use JSON::XS;
-use LWP::UserAgent;
-use YAML::XS qw(LoadFile DumpFile);
-
 use parent 'WebService::Netatmo::Common';
 
 sub BUILD {
