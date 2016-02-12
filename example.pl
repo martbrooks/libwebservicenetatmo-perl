556c2a3cbcfa5fbd9ca79f59637f5e0fd2474bb2
Martin A. Brooks <martin@hinterlands.org>
2016-02-12 21:56:27 +0000

    trim down example.

--- example.pl
+++ example.pl
@@ -6,9 +6,7 @@ use warnings;
 use utf8;
 
 use Cwd qw(abs_path);
-use Data::Dumper;
 use File::Basename qw(dirname);
-use Getopt::Long::Descriptive;
 use YAML::XS qw(LoadFile);
 
 use lib dirname( abs_path $0) . '/lib';
@@ -16,35 +14,14 @@ use WebService::Netatmo::WeatherStation;
 
 binmode STDOUT, ':utf8';
 
-my ( $opt, $usage ) = describe_options(
-    "%c %o",
-
-    ['Metrics:'],
-    [
-        'metric|m=s' => hidden => {
-            one_of => [
-                [ 'co2|c'         => 'Report CO2 readings.' ],
-                [ 'humidity|h'    => 'Report humidity readings.' ],
-                [ 'temperature|t' => 'Report temperature readings.' ],
-                [ 'noise|n'       => 'Report noise readings.' ],
-                [ 'pressure|p'    => 'Report pressure readings.' ],
-
-            ]
-        }
-    ],
-    [],
-);
 
 my $yaml = LoadFile("settings.yaml");
-
 my $client_id     = $yaml->{client_id};
 my $client_secret = $yaml->{client_secret};
 my $username      = $yaml->{username};
 my $password      = $yaml->{password};
 my $tokenstore    = './tokenstore.yaml';
 my $debug         = 0;
-
-my $metric = $opt->metric // 'temperature';
 
 my $netatmo = WebService::Netatmo::WeatherStation->new(
     client_id     => $client_id,
@@ -64,6 +41,7 @@ my %co2          = $netatmo->co2;
 
 foreach my $station ( sort keys %stations ) {
     my $stationname = $stations{$station}{station_name};
+    say "- $stationname";
     foreach my $module ( sort keys %{ $stations{$station}{submodules} } ) {
         my $modulename = $stations{$station}{submodules}{$module}{module_name};
         my $sensor;
@@ -72,7 +50,8 @@ foreach my $station ( sort keys %stations ) {
         my $humidity    = $humidities{$stationname}{$modulename}{pretty}   // '-';
         my $noiselevel  = $noise{$stationname}{$modulename}{pretty}        // '-';
         my $co2level    = $co2{$stationname}{$modulename}{pretty}          // '-';
-        say "$modulename: $temperature, $pressure, $humidity, $noiselevel, $co2level";
+        say "-- $modulename: $temperature, $pressure, $humidity, $noiselevel, $co2level";
     }
+    print "\n";
 }
 
